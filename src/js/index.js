6470305906d1f94527b8e14c16ae3a51200e78bf
SErgey Shelkovskiye <kadetlepel@mail.ru>
2022-06-14 19:56:07 +0300

    training js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world')
+// console.log('Hello world')
+
+// const title = document.createElement('h1');
+// const text = document.createTextNode('Hello World');
+// title.append(text);
+// document.body.append(title);
+// title.className = 'main-title';
+// title.id = 'title-id'
