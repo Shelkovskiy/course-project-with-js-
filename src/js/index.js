223aadca034ab2fa2b24920470a71864df229f81
SErgey Shelkovskiye <kadetlepel@mail.ru>
2022-06-29 21:07:24 +0300

    clean

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,1 @@
-// console.log('Hello world')
-
-// const title = document.createElement('h1');
-// const text = document.createTextNode('Hello World');
-// title.append(text);
-// document.body.append(title);
-// title.className = 'main-title';
-// title.id = 'title-id'
+'use strict'
