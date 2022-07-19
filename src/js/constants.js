a211f47cbf14d792a6f5326ef45863aff4c16561
SErgey Shelkovskiye <kadetlepel@mail.ru>
2022-07-19 13:25:14 +0300

    test push

---
+++ src/js/constants.js
@@ -0,0 +1,11 @@
+export const BOARD_1_KEY = 'BOARD_1';
+export const BOARD_2_KEY = 'BOARD_2';
+export const BOARD_3_KEY = 'BOARD_3';
+export const cardToLocal_1 = JSON.parse(localStorage.getItem(BOARD_1_KEY)) ;
+export const cardToLocal_2 = JSON.parse(localStorage.getItem(BOARD_2_KEY)) ;
+export const cardToLocal_3 = JSON.parse(localStorage.getItem(BOARD_3_KEY)) ;
+export const btn_1 = document.getElementById('board-1')
+export const btn_2 = document.getElementById('board-2')
+export const btn_3 = document.getElementById('board-3')
+
+
---
+++ src/js/utils.js
@@ -0,0 +1,32 @@
+// Рисуем новые карточки
+export const displayCard = (cards) => {
+   if(cards) {
+    const searchCard = cards.map((card) =>{
+       return `
+      
+         <div class="card" id ="${card.id}">
+            <div class="card-img">
+               <img src="${card.src}"></img>
+            <div class="card-board">
+             <button class="addToBoard-1" id="addToBoard-1"> Добавить на доску №1
+             </button>
+             <button class="addToBoard-2" id="addToBoard-2"> Добавить на доску №2
+             </button>
+             <button class="addToBoard-3" id="addToBoard-3"> Добавить на доску №3
+             </button>
+            
+                </div>
+
+            </div>
+            <div class="card-title"> 
+                <h4> ${card.label} </h4>
+                   </div>
+          </div>
+        
+                      `;
+       }).join('');
+   
+   card_wrapper.innerHTML = searchCard;
+   
+  }
+} 
